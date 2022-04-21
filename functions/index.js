ec4150ce2e7b476fc6244e2d60a924a9e5ea79ba
PandaPalumbo <mattdpalumbo@gmail.com>
2022-04-20 20:10:30 -0400

    getuser and UpdateUsre working. can run locally in postman

--- functions/index.js
+++ functions/index.js
@@ -11,20 +11,61 @@ const prisma = new PrismaClient()
 //User Related
 //************ */
 
-exports.getUserData = functions.https.onRequest(async (request, response) => {
-    functions.logger.error(`body: ${JSON.stringify(request.body)}`);
-    let { email } = request.body;
-    functions.logger.error(`Email: ${email}`);
+const getUser = functions.https.onRequest(async (request, response) => {
+    functions.logger.log(`body: ${request.body}`);
+    const { email } = request.body;
     try {
         const user = await prisma.users.findUnique({
             where: {
-                userId: 1
+                email
+            },
+            include:{
+                user_favorite_places: true,
+                user_friends: true,
             }
         })
         response.json(user);
+        return user
     }
     catch(error) {
         functions.logger.error(`Error: ${error.message}`);
         response.json(error);
     }
 });
+
+const updateUser = functions.https.onRequest(async (request, response) => {
+    functions.logger.log(`body: ${request.body}`);
+    const { email } = request.body;
+    try {
+        const user = await prisma.users.upsert({
+            where: {
+                email
+            },
+            update: request.body,
+            create: request.body,
+        })
+        response.json(user);
+        return user
+    }
+    catch(error) {
+        functions.logger.error(`Error: ${error.message}`);
+        response.json(error);
+    }
+});
+
+// eslint-disable-next-line consistent-return
+const index = (req, res) => {
+    switch (req.path) {
+        case '/getUser':
+            return getUser(req, res)
+        case '/updateUser':
+            return updateUser(req, res)
+        default:
+            res.send('function not defined')
+    }
+}
+
+module.exports = {
+    index,
+    getUser
+}
